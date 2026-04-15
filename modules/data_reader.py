46f2a4b37e674ee78a307ef6d4a772743b1e2981
Galo <galosoulard3@gmail.com>
2026-04-15 12:24:10 -0300

    Se agrega: Función del ejercicio 2.B 'get_column_names() en archivo data_reader.py, y demostración del mismo en ejercicio2.ipynb'

--- modules/data_reader.py
+++ modules/data_reader.py
@@ -2,9 +2,8 @@ import csv
 
 def print_first_rows(file_path, file_delimiter):
     """
-    Abre el archivo principal del dataset con la librería csv 
-    e imprime las primeras 10 filas para verificar que
-    fue abierto correctamente.
+    Abre el archivo principal del dataset e imprime las primeras 
+    10 filas para verificar que fue abierto correctamente.
     
     Args:
         file_path (str): La ruta del archivo a trabajar.
@@ -19,3 +18,22 @@ def print_first_rows(file_path, file_delimiter):
             if index >= 10:
                 break
             print(f"Fila {index + 1}: {row}\n")
+
+
+
+def get_column_names(file_path, file_delimiter):
+    """
+    Lee el archivo principal del dataset y retorna una lista 
+    con los nombres de todas sus columnas.
+    
+    Args:
+        file_path (Path/str): La ruta del archivo a trabajar.
+        file_delimiter (str): El caracter que separa las columnas.
+    """
+    with open(file_path, mode='r', encoding='UTF-8') as file:
+        reader = csv.reader(file, delimiter=file_delimiter)
+        
+        #Avanzamos a la primera posición, en donde se encuentran los headers
+        headers = next(reader)
+        
+        return headers
